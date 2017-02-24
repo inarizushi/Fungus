7b44a4c7f97e461130d6c4dc0ba66cb0f5cb86cb
lealeelu <leah.lee.42@gmail.com>
2017-02-24 11:19:31 -0500

    Add ease

--- Assets/Fungus/Scripts/Components/SaveMenu.cs
+++ Assets/Fungus/Scripts/Components/SaveMenu.cs
@@ -200,7 +200,9 @@ namespace Fungus
             if (saveMenuActive)
             {
                 // Switch menu off
-                LeanTween.value(saveMenuGroup.gameObject, saveMenuGroup.alpha, 0f, 0.2f).setOnUpdate( (t) => {
+                LeanTween.value(saveMenuGroup.gameObject, saveMenuGroup.alpha, 0f, 0.2f)
+                    .setEase(LeanTweenType.easeOutQuint)
+                    .setOnUpdate( (t) => {
                     saveMenuGroup.alpha = t;
                 }).setOnComplete( () => {
                     saveMenuGroup.alpha = 0f;
@@ -209,7 +211,9 @@ namespace Fungus
             else
             {
                 // Switch menu on
-                LeanTween.value(saveMenuGroup.gameObject, saveMenuGroup.alpha, 1f, 0.2f).setOnUpdate( (t) => {
+                LeanTween.value(saveMenuGroup.gameObject, saveMenuGroup.alpha, 1f, 0.2f)
+                    .setEase(LeanTweenType.easeOutQuint)
+                    .setOnUpdate( (t) => {
                     saveMenuGroup.alpha = t;
                 }).setOnComplete( () => {
                     saveMenuGroup.alpha = 1f;
